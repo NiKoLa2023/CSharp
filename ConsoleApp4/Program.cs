f50db5c3cee14c78f3fae826512c69ab27d2312a
NiKoLa2023 <150241393+NiKoLa2023@users.noreply.github.com>
2024-03-06 13:50:56 +0100

    Add files via upload

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter firstNumber");
+string firstNumberInput = Console.ReadLine();
+Console.WriteLine("Enter secondNumber");
+string secondNumberInput = Console.ReadLine();
+Console.WriteLine("Enter thirdNumber");
+string thirdNumberInput = Console.ReadLine();
+Console.WriteLine("Enter fourthNumber");
+string fourthNumberInput = Console.ReadLine();
+
+bool myfirstInputResult = int.TryParse(firstNumberInput, out int firstConversionInput);
+bool mysecondInputResult = int.TryParse(secondNumberInput, out int secondConversionInput);
+bool mythirdInputResult = int.TryParse(thirdNumberInput, out int thirdConversionInput);
+bool myfourthInputResult = int.TryParse(fourthNumberInput, out int fourthConversionInput);
+double finalResult = (double)(firstConversionInput + secondConversionInput + thirdConversionInput + fourthConversionInput) / 4;
+
+if (myfirstInputResult && mysecondInputResult && mythirdInputResult && myfourthInputResult)
+{
+    Console.WriteLine("The average of " + firstConversionInput + "," + secondConversionInput + "," + thirdConversionInput + "," + fourthConversionInput + " is:" + finalResult);
+}
+else
+{
+    Console.WriteLine("PLease enter valid number");
+}
+
