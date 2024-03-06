cfaf6a665965a0c2fdfa7ee7ca0dbc1364986b67
NiKoLa2023 <150241393+NiKoLa2023@users.noreply.github.com>
2024-03-06 14:10:57 +0100

    Add files via upload

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int FirstNumber = 10;
+
+int SecondNumber = 4;
+
+int freeze = FirstNumber;
+
+FirstNumber = SecondNumber;
+SecondNumber = freeze;
+
+Console.WriteLine(FirstNumber);
+Console.WriteLine(SecondNumber);
+
+
+
