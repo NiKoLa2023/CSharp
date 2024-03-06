f0fe814b463e452aa87be2b46b3d29b6194c01f3
NiKoLa2023 <150241393+NiKoLa2023@users.noreply.github.com>
2024-03-06 13:15:44 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿
+Console.WriteLine("Enter firstNumber");
+string FirstNumber = Console.ReadLine();
+Console.WriteLine("Enter secondNumber");
+string SecondNumber = Console.ReadLine();
+Console.WriteLine("Operator");
+string operatorF = Console.ReadLine();
+
+bool myfirstNumber = int.TryParse(FirstNumber, out int FirstConversion);
+bool mysecondNumber = int.TryParse(SecondNumber, out int SecondConversion);
+int result = FirstConversion + SecondConversion;
+int secondResult = FirstConversion - SecondConversion;
+int thirdrResult = FirstConversion * SecondConversion;
+double fourthrResult = (double)FirstConversion / SecondConversion;
+
+if (myfirstNumber && mysecondNumber)
+{
+    switch (operatorF)
+    {
+        case "+": Console.WriteLine("Result is " +  result); break;
+        case "-": Console.WriteLine("Result is " + secondResult); break;
+        case "*": Console.WriteLine("Result is " + thirdrResult); break;
+        case "/": Console.WriteLine("Result is " + fourthrResult); break;
+        default: Console.WriteLine("Not appropriate operator");break;
+
+    }
+}
+else
+{
+    Console.WriteLine("Wrong input");
+}
+
+
+
+
+
