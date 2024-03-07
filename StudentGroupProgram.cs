d3c3f44dd4576c0493720e2219e6f81a6d2d0011
NiKoLa2023 <150241393+NiKoLa2023@users.noreply.github.com>
2024-03-07 13:46:46 +0100

    Add files via upload

---
+++ StudentGroupProgram.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Please enter numberGroup");
+bool firstSuccess = int.TryParse(Console.ReadLine(), out int firstNumber);
+if (firstSuccess && (firstNumber == 1 || firstNumber == 2))
+{
+    firstSuccess = true;
+}
+else { firstSuccess = false; }
+
+
+string[] studentsG1 = new string[] { "Zdravko", "Petko", "Stanko", "Branko", "Trajko" };
+string[] studentsG2 = new string[] { "Nikola", "Angelina", "MAxim ", "Luka", "Gogo" };
+
+
+if (firstSuccess)
+{
+    if (firstNumber == 1)
+    {
+        for (int i = 0; i < studentsG1.Length; i++)
+        {
+            Console.WriteLine(studentsG1[i]);
+        }
+    }
+    if (firstNumber == 2)
+        {
+            for (int i = 0; i < studentsG2.Length; i++)
+            {
+                Console.WriteLine(studentsG2[i]);
+            }
+        }
+    }
+
+else { Console.WriteLine("Please enter 1 or 2"); }
