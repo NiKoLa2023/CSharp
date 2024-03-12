c101bba8b6c58f74c0d744b7391ae539415453a7
NiKoLa2023 <150241393+NiKoLa2023@users.noreply.github.com>
2024-03-12 14:30:40 +0100

    Add files via upload

---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+//Task 1//
+
+using System.Runtime.ExceptionServices;
+
+Console.WriteLine("PLease enter simple string");
+string input = Console.ReadLine();
+
+
+int lenghtInput = input.Length - 5;
+
+string reducedNikola = input.Substring(lenghtInput, 5); //
+Console.WriteLine(reducedNikola);
+
+
+
+//Task 2 Take a sentence as input and print its words.//
+Console.WriteLine("PLease enter sentence");
+string inputMessage = Console.ReadLine();
+
+
+string[] words = inputMessage.Split(" ");
+
+
+
+foreach (string word in words)
+{
+    Console.WriteLine(word);
+}
+
+
+//Task 3 Create a function that takes a number as input. This method should return the sum of the digits in the number//
+
+
+Console.WriteLine("PLease enter number");
+bool sucees = int.TryParse(Console.ReadLine(), out int inputThird);
+
+if (sucees) 
+{ Console.WriteLine(Printinfo(inputThird));}
+
+else { Console.WriteLine("Please eneter valid number"); }
+
+    int Printinfo(int number)
+    {
+        int sum = 0;
+        while (inputThird != 0)
+        {
+            int R = inputThird % 10;
+            sum += R;
+            inputThird = inputThird / 10;
+        }
+
+        return sum;
+
+    }
+
+
+    
