1ced2e0c0c045e922a8abce54a182840286802c0
NiKoLa2023 <150241393+NiKoLa2023@users.noreply.github.com>
2024-03-19 11:47:56 +0100

    Add files via upload

---
+++ Homework5/Classess/Dogcs.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Homework5.Classess
+{
+    public class Dog
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Color { get; set; }
+
+        public void PrintEat()
+        {
+            Console.WriteLine($"The dog {Name} is now eating");
+        }
+        public void PrintPlay()
+        {
+            Console.WriteLine($"The dog {Name} is now playing");
+
+        }
+        public void ChaseTail()
+        {
+            Console.WriteLine($"The dog {Name} is now chasing its tail");
+
+        }
+    }
+}
+
---
+++ Homework5/Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Homework5.Classess;
+
+Dog dogName = new Dog();
+
+while (true)
+{
+    Console.WriteLine("Please enter name");
+    dogName.Name = Console.ReadLine();
+    Console.WriteLine("Please choose the message 1.PrintEat /n 2.PrintPlay /n 3.ChaseTail");
+    int input = int.Parse(Console.ReadLine());
+    if(input == 1)
+    {
+        dogName.PrintEat();
+    }
+    else if(input == 2) {
+        dogName.PrintPlay();
+    }
+    else if (input == 3)
+    {
+        dogName.ChaseTail();
+    }
+    else { Console.WriteLine("Please chooese from 1 to 3"); }
+}
