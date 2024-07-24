848f83b0686b64503bb2e165f8b7ff7aec6c28ed
NiKoLa2023 <150241393+NiKoLa2023@users.noreply.github.com>
2024-07-24 13:23:20 +0200

    Add files via upload

---
+++ PizzaAppRefactored.Services/Implementations/UserService.cs
@@ -0,0 +1,31 @@
+﻿using PizzaAppRefacored.DataBase;
+using PizzaAppRefactored.Domain.Models;
+using PizzaAppRefactored.Mappers;
+using PizzaAppRefactored.Services.Interfaces;
+using PizzaAppRefactored.ViewModels.UserViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaAppRefactored.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        private IRepository<User> _userRepository;
+
+        public UserService(IRepository<User> userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public List<UserOptionViewModel> GetAllUsersForDropdown()
+        {
+            List<User> users = _userRepository.GetAll();
+
+            List<UserOptionViewModel> userOptionViewModels = users.Select(x => UserMapper.ToUserOptionViewModel(x)).ToList();
+
+            return userOptionViewModels;
+        }
+    }
+}
